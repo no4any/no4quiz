b1951492fc3a5e4f786162f2bd8e2d26ead2aaec
Martin Karry <martin@karry.de>
2023-01-21 01:40:24 +0100

    Added all exports to typeguards

--- packages/common/src/quiz/typeguards/index.ts
+++ packages/common/src/quiz/typeguards/index.ts
@@ -1,2 +1,5 @@
 export * from "./isQuizQuestionType.typeguard"
 export * from "./isQuizQuestion.typeguard"
+export * from "./isQuizQuestionSingle.typeguard"
+export * from "./isQuizQuestionSingleAnswer.typeguard"
+export * from "./isQuizQuestionText.typeguard"
